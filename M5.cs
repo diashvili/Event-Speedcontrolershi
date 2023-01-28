3af0186f17c4f5a044a46b091be3ccef363afe23
davit iashvili <diashvili3@gmail.com>
2023-01-28 05:38:34 -0800

    Add files via upload

---
+++ M5.cs
@@ -0,0 +1,51 @@
+﻿using G15;
+
+namespace BMW;
+
+internal class M5
+{
+	public const int MaxSpeed = 320;
+
+	private readonly SpeedController _speedController;
+
+	public M5()
+	{
+		_speedController = new SpeedController();
+		// Subscribing event
+		//_speedController.SpeedExceed += Alarm;
+		_speedController.SpeedExceed += x => Console.WriteLine($"Please, slow down by {x}");
+	}
+
+	public string Model => "BMW M5";
+	public int CurrentSpeed { get; private set; }
+
+	public void Accelerate(int kmh)
+	{
+		if (CurrentSpeed + kmh < MaxSpeed && CurrentSpeed + kmh >= 0)
+		{
+			CurrentSpeed += kmh;
+		}
+		else if(CurrentSpeed + kmh <= 0)
+        {
+			CurrentSpeed = 0;
+        }
+		else
+		{
+			CurrentSpeed = MaxSpeed;
+		}
+
+		_speedController.CurrentSpeed = this.CurrentSpeed;
+
+		Console.WriteLine($"{Model} Current Speed Is {CurrentSpeed}");
+	}
+
+	private void Alarm(int x)
+	{
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.WriteLine($"Your Speed Is To High! Over Speed Is: {x}");
+		Console.ResetColor();
+		// Unsubscribing event
+		_speedController.SpeedExceed -= Alarm;
+
+	}
+}
---
+++ SpeedController.cs
@@ -0,0 +1,35 @@
+﻿namespace G15;
+
+internal class SpeedController
+{
+	private int _currentSpeed;
+
+	// Publishing event
+	public event Action<int> SpeedExceed;
+
+	public SpeedController()
+	{
+		MaxSpeed = Random.Shared.Next(200, 300);
+	}
+
+	public int MaxSpeed { get; private init; }
+
+	public int CurrentSpeed
+	{
+		get
+		{
+			return _currentSpeed;
+		}
+		set
+		{
+			if (value > MaxSpeed)
+			{
+				if (SpeedExceed != null)
+				{
+					SpeedExceed(value - MaxSpeed);
+				}
+			}
+			_currentSpeed = value;
+		}
+	}
+}
